281b9b72ded261e964a78c51312a4cbc2135b530
ggranata <gabriele.granata@dbinformation.it>
2022-03-24 12:11:09 +0100

    Add rename for single pages, on tuttosport and corrieresport

--- src/main/java/com/mimesi/ftpdrive/service/RenameFTPDriveService.java
+++ src/main/java/com/mimesi/ftpdrive/service/RenameFTPDriveService.java
@@ -10,6 +10,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -33,13 +37,89 @@ public class RenameFTPDriveService {
 
         // Creiamo la lista generica che verrà popolata con
         // la lista dei file, suddivisi per le varie edizioni
+        final String pdfToPathRename = pdfToPathRename(idFonte);
         final List<List<File>> listFontePDF = setEditionSourcesPDF(fromPathFiles,idFonte);
+        if (idFonte.equals(872)){
+            for (List<File> edition : listFontePDF) {
+                for(File singleFile : edition){
+                    if(singleFile.getName().substring(21,22).equals("S") ||
+                            singleFile.getName().substring(21,22).equals("T") ||
+                            singleFile.getName().substring(21,22).equals("Z") ){
+                        logger.info("File: "+singleFile.getName());
+                        logger.info("PAGINA DA CONSIDERARE DOPPIA, DEVE ESSERE RITAGLIATA");
+                    } else {
+                        try {
+                            logger.info("PAGINA TUTTOSPORT SINGOLA, STO PER RINOMINARE E COPIARE");
+                            final Path fromPath = Paths.get(singleFile.getAbsolutePath());
+                            final String numPageFile = singleFile.getName().substring(16, 18);
+                            StringBuilder fileFinalName = new StringBuilder("0" +idFonte+ "_binpage");
+                            if (singleFile.getName().matches(FTPConst.REGEX_TUTTOSPORT_NAZIONALE)) {
+                                fileFinalName.append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_TUTTOSPORT_PIEMONTE)){
+                                fileFinalName.append(FTPConst.EDIZIONE_PIEMONTE).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_TUTTOSPORT_SICILIA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_SICILIA).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_TUTTOSPORT_SARDEGNA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_SARDEGNA).append(numPageFile).append(".pdf");
+                            } else {
+                                logger.error("Il file "+singleFile.getName()+ "non matcha con nessun edizione della fonte TUTTOSPORT");
+                            }
+                            final Path finalBatchFolder = Paths.get(pdfToPathRename+File.separatorChar+fileFinalName);
+                            Files.copy(fromPath,finalBatchFolder);
+                        } catch (IOException ex) {
+                            logger.error("Errore durante la copia del file rinominato");
+                            logger.error(ex.getMessage());
+                        }
+                    }
+                }
+            }
+        } else if (idFonte.equals(873)){
+            for (List<File> edition : listFontePDF) {
+                for(File singleFile : edition){
+                    if(singleFile.getName().substring(21,22).equals("S") ||
+                            singleFile.getName().substring(21,22).equals("T") ||
+                            singleFile.getName().substring(21,22).equals("Z") ){
+                        logger.info("File: "+singleFile.getName());
+                        logger.info("PAGINA DA CONSIDERARE DOPPIA, DEVE ESSERE RITAGLIATA");
+                    } else {
+                        try {
+                            logger.info("PAGINA SINGOLA, STO PER RINOMINARE E COPIARE");
+                            final Path fromPath = Paths.get(singleFile.getAbsolutePath());
+                            final String numPageFile = singleFile.getName().substring(16, 18);
+                            StringBuilder fileFinalName = new StringBuilder("0" +idFonte+ "_binpage");
+                            if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_NAZIONALE)) {
+                                fileFinalName.append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_CAMPANIA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_CAMPANIA).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_PUGLIA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_PUGLIA).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_ROMA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_ROMA).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_SARDEGNA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_SARDEGNA).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_VENETO)){
+                                fileFinalName.append(FTPConst.EDIZIONE_VENETO).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_STADIO)){
+                                fileFinalName.append(FTPConst.EDIZIONE_STADIO).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_STADIO_BOLOGNA)){
+                                fileFinalName.append(FTPConst.EDIZIONE_STADIO_BOLOGNA).append(numPageFile).append(".pdf");
+                            } else if (singleFile.getName().matches(FTPConst.REGEX_CORRSPORT_SUPPLEMENTO)){
+                                fileFinalName.append(FTPConst.EDIZIONE_SUPPLEMENTO).append(numPageFile).append(".pdf");
+                            } else {
+                                logger.error("Il file "+singleFile.getName()+ "non matcha con nessun edizione della fonte CORRIERE SPORT");
+                            }
+                            final Path finalBatchFolder = Paths.get(pdfToPathRename+File.separatorChar+fileFinalName);
+                            Files.copy(fromPath,finalBatchFolder);
+                        } catch (IOException ex) {
+                            logger.error("Errore durante la copia del file rinominato");
+                            logger.error(ex.getMessage());
+                        }
 
-        for (List<File> edition : listFontePDF) {
-            for(File singleFile : edition){
-
+                    }
+                }
             }
         }
+
 
 
         return responseDto;
@@ -82,7 +162,7 @@ public class RenameFTPDriveService {
                 }
             }
 
-            logger.info("ADDING LIST OF EDITION IN FATHER LIST FOR CORR.SPORT");
+            logger.info("ADDING LIST OF EDITION IN FATHER LIST FOR TUTTO SPORT");
             listFileSource.add(listFileNazionale);
             listFileSource.add(listFilePiemonte);
             listFileSource.add(listFileSicilia);
@@ -144,5 +224,27 @@ public class RenameFTPDriveService {
         }
 
         return listFileSource;
+    }
+
+    /**
+     * Method for get path of source in wich we have a renamed file
+     * @param idFonte
+     * @return
+     */
+    private String pdfToPathRename (final Integer idFonte) {
+        String pdfToPathRename = "";
+
+        switch (idFonte) {
+            case 872:
+                pdfToPathRename = env.getProperty("parent.folder.pdf") + File.separatorChar
+                        + env.getProperty("pattern.tuttosport.marge.872");
+                break;
+            case 873:
+                pdfToPathRename = env.getProperty("parent.folder.pdf") + File.separatorChar
+                        + env.getProperty("pattern.corrieresport.marge.873");
+                break;
+        }
+
+        return pdfToPathRename;
     }
 }
