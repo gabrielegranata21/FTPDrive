f545f97bfa8ec7c6bcc33751c6f0b74826639064
ggranata <gabriele.granata@dbinformation.it>
2022-03-30 15:46:20 +0200

    Add Thred pool schedule task for schedule jobs

---
+++ src/main/java/com/mimesi/ftpdrive/configuration/ThreadPoolTaskSchedulerConfig.java
@@ -0,0 +1,23 @@
+package com.mimesi.ftpdrive.configuration;
+
+import com.mimesi.ftpdrive.schedule.FtpSchedule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@ComponentScan(basePackages = "com.mimesi.ftpdrive.schedule",
+        basePackageClasses = {FtpSchedule.class})
+public class ThreadPoolTaskSchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler
+                = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix(
+                "ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+}
